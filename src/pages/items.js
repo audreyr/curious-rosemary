2376aeac1a4baeb1627ccbdc90c15af2fb57561e
Audrey Feldroy <audrey@feldroy.com>
2020-04-21 19:20:43 -0700

    Create simple items page for listview experiment

---
+++ src/pages/items.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default () => (
+    <div>
+        <h1>Items List</h1>
+        <p>This is a static site, but it's loading items dynamically:</p>
+        <ul>
+            <li>TODO Item 1</li>
+            <li>TODO Item 2</li>
+        </ul>
+    </div>
+)
