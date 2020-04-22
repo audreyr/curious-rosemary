4d3e6fe687c1c85e26107cac2e87985aaa27d075
Audrey Feldroy <audrey@feldroy.com>
2020-04-21 20:10:09 -0700

    Populate list with GraphQL nav links

--- src/pages/items.js
+++ src/pages/items.js
@@ -1,11 +1,24 @@
 import React from "react"
-export default () => (
+export default ({ data }) => (
     <div>
-        <h1>Items List</h1>
-        <p>This is a static site, but it's loading items dynamically:</p>
+        <h1>Items List {data.site.siteMetadata.title}</h1>
+        <p>This is a static site, but it's loading items dynamically from GraphQL:</p>
         <ul>
-            <li>TODO Item 1</li>
-            <li>TODO Item 2</li>
+          {data.site.siteMetadata.header.nav_links.map(x=> <li>{x.label}</li>)}
         </ul>
     </div>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        header {
+          nav_links {
+            label
+          }
+        }
+      }
+    }
+  }
+`
